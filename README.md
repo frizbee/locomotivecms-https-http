5c6257b68bd5e9e95adef053b1a90de029a08fc5
Andre <tooandre@gmail.com>
2019-08-23 18:14:30 +0800

    add domready

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # LocomotiveCMS https to http
 
+Fof personal use
 Force replace sign in url from https to http
 
 
---
+++ js/popup.js
@@ -0,0 +1,23 @@
+domReady(() => {
+    bindCheckboxes()
+    // initRateButton()
+})
+
+function domReady (callback) {
+    if (document.readyState === 'complete') {
+      callback()
+    } else {
+      window.addEventListener('load', callback, false);
+    }
+  }
+
+function bindCheckboxes() {
+    for (const $setting of document.querySelectorAll('.setting')) {
+      const $input = $setting.querySelector('input')
+      $input.checked = localStorage[$input.name] === 'true'
+      $setting.addEventListener('change', (event) => {
+        localStorage[$input.name] = $input.checked
+      }, false)
+      console.log($input.checked)
+    }
+}
