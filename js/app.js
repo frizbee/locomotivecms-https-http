74904674d7eac0e3767b9fad78101258f2d1370f
Andre Best <tooandre@gmail.com>
2019-11-01 15:31:31 +0800

    update https to http from account pass chage form

--- js/app.js
+++ js/app.js
@@ -11,8 +11,12 @@ var hosts = ['bursadev.shareinvestor.com','localhost:3000']
  * */
 if(hosts.indexOf(host)) {
   let form = document.querySelector('#new_locomotive_account');
+  let accountChangeForm = document.querySelector('.edit_account');
   if(form) {
     let new_action = form.action.replace(/^https:\/\//i, 'http://');
     form.action = new_action;
+  }else if(accountChangeForm) {
+    let new_action = accountChangeForm.action.replace(/^https:\/\//i, 'http://');
+    accountChangeForm.action = new_action;
   }
 }
