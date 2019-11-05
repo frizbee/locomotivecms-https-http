ee05ed218844b3f460dba814c43cd9a3e1891ffa
frizbee <mail2andrewa@gmail.com>
2019-11-05 10:39:35 +0800

    fix typo

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var hosts = ['bursadev.shareinvestor.com','localhost:3000']
  * If host in array of hosts
  * then do the modification
  * */
-if(hosts.indexOf(host)) {
+if(hosts.indexOf(host) >= 0) {
   let form = document.querySelector('#new_locomotive_account');
   let accountChangeForm = document.querySelector('.edit_account');
   if(form) {
